1731ce86af9303547467ffb956027faaddb6bece
AntonioJesusBlanco <ablagui1506@g.educaand.es>
2025-05-27 19:48:11 +0200

    Front and back conected, json updated

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const datos = require('./datos.json');
+const personajes = require('./datos.json');
 
 app.use(cors());
 
-app.get('/api/usuarios', (req, res) => {
-  res.json(datos);
+app.get('/api/personajes', (req, res) => {
+  res.json(personajes);
 });
 
 app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
